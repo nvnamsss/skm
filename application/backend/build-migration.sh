f721b108bd722d30224c6478d8016210d7b08aaa
namvn <nam.nguyen10@onemount.com>
2022-04-07 11:35:50 +0700

    added migratio

--- application/backend/build-migration.sh
+++ application/backend/build-migration.sh
@@ -1,6 +1,6 @@
 #/bin/bash
-image_name=skm
+image_name=skm-migration
 echo "REMOVING OLD IMAGE"
 docker image rm -f $image_name
 echo "BUILDING NEW IMAGE"
-docker build . -t $image_name
+docker build . -f Dockerfile-migration -t $image_name
