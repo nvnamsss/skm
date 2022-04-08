9a2552ab1fdcf9c20f0857191908f10ba540719b
namvn <nam.nguyen10@onemount.com>
2022-04-08 23:25:40 +0700

    update configuration

--- application/frontend/src/shares/axios.ts
+++ application/frontend/src/shares/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
   // baseURL: "http://ec2-13-250-113-138.ap-southeast-1.compute.amazonaws.com",
+  baseURL: import.meta.env.VITE_FAMOUS_QUOTES_HOST,
   headers: {
     'Access-Control-Allow-Origin' : 'origin',
     // 'Origin': 'localhost',
